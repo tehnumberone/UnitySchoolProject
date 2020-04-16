949f1b3b9b615309aa25689cd6d97193953945cd
Emre Dogan <47520970+tehnumberone@users.noreply.github.com>
2020-04-16 10:27:14 +0200

    setup

---
+++ Assets/Scripts/InteractableItemBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItemBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string Name;
+
+    public string InteractText = "Press F to pick up the item";
+
+    public virtual void OnInteract()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Kaas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kaas : InteractableItemBase
+{
+    private bool isPickedUp = false;
+    public override void OnInteract()
+    {
+        InteractText = "Press F to";
+
+        isPickedUp = !isPickedUp;
+
+        InteractText += isPickedUp ? "to drop" : "to pickup";
+
+        GetComponent<Animator>().SetBool("Pickedup", isPickedUp);
+    }
+}
